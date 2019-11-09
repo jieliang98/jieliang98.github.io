10ead76b990265d364bfd9d97ef9ec26b2e8e5d7
jieliangang <jieliangang@gmail.com>
2019-11-09 18:41:15 -0500

    Add new stuff

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var x = document.getElementById("navigation-hamburger");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
